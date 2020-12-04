5e6310eb51a86685e2b75581ddb507dfea656d48
Yohei Tsutsumi <tsutsumi@acoustics.co.jp>
2020-12-04 15:33:40 +0900

    設定画面追加

--- development/assets/scripts/flash.js
+++ development/assets/scripts/flash.js
@@ -1,14 +1,45 @@
 $(function(){
-    var total = 20;
-    var item = 5;
-    var answer = 0;
+    var maxNumber = 20;
+    var minNumber = 5;
+    var counts = 5;
+    var time = 10;
+    var speed;
+    var answer;
+    getSettings();
 
-    //itemの個数の数字が入ったtotalの配列
+    //countsの個数の数字が入ったmaxNumberの配列
     var Rary = [];
-    //itemの個数の配列 
+    //countsの個数の配列 
     var Tary =[];
 
-    $('#startButton, #replayButton').on('click', function(){
+    function saveSettings(){
+        localStorage.setItem('maxNumber', $('#maxNumber').val());
+        localStorage.setItem('minNumber', $('#minNumber').val());
+        localStorage.setItem('counts', $('#counts').val());
+        localStorage.setItem('time', $('#time').val());
+    }
+
+    function getSettings(){
+        maxNumber = parseInt(localStorage.getItem('maxNumber'));
+        minNumber = parseInt(localStorage.getItem('minNumber'));
+        counts = parseInt(localStorage.getItem('counts'));
+        time = parseInt(localStorage.getItem('time'));
+        $('#maxNumber').val(maxNumber);
+        $('#minNumber').val(minNumber);
+        $('#counts').val(counts);
+        $('#time').val(time);
+        speed = time * 1000 / counts;
+    }
+
+    $('#startButton').on('click', function(){
+        saveSettings();
+        getSettings();
+        $('#settings').hide();
+        countdown();
+        return false;
+    });
+
+    $('#replayButton').on('click', function(){
         countdown();
         return false;
     });
@@ -39,16 +70,23 @@ $(function(){
             },100);
             $('#countNumber').text(Tary[counter]);
             counter++;
-            if (counter === item) {
+            if (counter === counts) {
                 clearInterval(timer);
                 setTimeout(function(){
                     $('#answerButtonArea').show();
                 },100);
             }
-        }, 1000);
+        }, speed);
     }
     $('#answerButton').on('click', function(){
         showAnswerFlash();
+    });
+
+    $('#resetButton').on('click', function(){
+        $('#countNumber').text('');
+        $('#settings').show();
+        $('#startButtonArea').show();
+        $('#replayButtonArea').hide();
     });
 
     function showAnswerFlash(){
@@ -59,24 +97,24 @@ $(function(){
     }
 
     function setNumbers(){
-        answer = Math.floor(Math.random()*(total - item)) + item;
+        answer = Math.floor(Math.random()*(maxNumber - minNumber)) + minNumber;
 
-        //itemの個数分、0が用意された配列をつくる。
-        //今回は配列にitemの数4つ分、0が入った配列を準備。
-        for(var i=0;i<item;i++){
+        //countsの個数分、0が用意された配列をつくる。
+        //今回は配列にcountsの数4つ分、0が入った配列を準備。
+        for(var i=0;i<counts;i++){
             Tary[i] = 0;
         }
 
-        //0からitemの個数が乱数で入ったtotalの数分の配列をつくる
-        //今回は0、1、2、3のいずれかの数が入ったtotalの数20個分の配列をつくる
+        //0からcountsの個数が乱数で入ったmaxNumberの数分の配列をつくる
+        //今回は0、1、2、3のいずれかの数が入ったmaxNumberの数20個分の配列をつくる
         for(var i=0;i<answer;i++){
-            Rary[i] = Math.floor(Math.random()*item);
+            Rary[i] = Math.floor(Math.random()*counts);
         }
 
-        //配列Raryに入ったitemの数字を確認し、該当する数字をTaryの配列に代入する
+        //配列Raryに入ったcountsの数字を確認し、該当する数字をTaryの配列に代入する
         for(var i=0;i<answer;i++){
-            //itemの配列の数の分繰り返し、該当する数字があれば配列の数字に1を加える
-            for(var j=0;j<item;j++){
+            //countsの配列の数の分繰り返し、該当する数字があれば配列の数字に1を加える
+            for(var j=0;j<counts;j++){
                 if(Rary[i] == j){
                     Tary[j]++;
                 }
